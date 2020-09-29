6711ec6c02b555dc28c4723b404d0ab2bb840778
felix24mihai <mihai_prava@yahoo.com>
2020-09-30 01:40:49 +0300

    2-pages

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-console.log("Hello world!!!aa!!");
 const express = require('express');
 const app = express();
 const http = require('http'); 
@@ -8,15 +7,18 @@ const path = require('path');
 const router = express.Router();
 const PORT = 3000; 
 
-// app.get('/', function(req, res){
-//    res.send("Hello world!!bbbbbb");
-// });
 app.use(express.static(__dirname + '/public'));
 
 //Load the first page
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/page.html'));
+    res.sendFile(path.join(__dirname+'/pages/firstPage.html'));
   });
+
+  //Load the second page
+app.get('/second',function(req,res){
+  res.sendFile(path.join(__dirname + '/pages/page2.html'));
+});
+
 
 //app.use('/', router);
 console.log('Running at Port 3000');
